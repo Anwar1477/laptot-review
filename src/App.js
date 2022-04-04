9208808cd3a394eb0145e4ed4582e827c4512fee
Anwar1477 <joy.cse017@gmail.com>
2022-04-04 16:53:31 +0600

    home page done

--- src/App.js
+++ src/App.js
@@ -7,15 +7,16 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import NotFound from "./components/NotFound/NotFound";
 import Review from "./components/Review/Review";
+import Images1 from "./Images/laptop.jpg"
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
 
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/" element={<Home image={Images1}></Home>}></Route>
+        <Route path="/home" element={<Home image={Images1}></Home>}></Route>
         <Route path="/review" element={<Review></Review>}></Route>
         <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Home.css'
 
-const Home = () => {
+
+
+const Home = ({image}) => {
+
     return (
-        <div>
-            <h1>This is Home Page</h1>
+        <div className='home-page'>
+            <div className='image'>
+                <img src={image} alt="" />
+            </div>
+            <div className='text-area'>
+                <h3 className='next'>Your Next Laptop </h3>
+                <h3 className='best'>Your Best Laptop</h3>
+                <p>Smile Laptop is a domestic product in Bangladesh. It’s a most innovative laptop that is introducing with some health care features too. Those health care features are Blood Pressure meter  Gloco-meter. Which are producing health care services easily  quickly with any doctors.</p>
+                <button className='demo-btn'>Choice Your Laptop</button>
+
+            </div>
+            
         </div>
     );
 };
